010da1d965b8bf560a2e458dbf19f605dc8d6633
Bogdan-Lishchynskiy <lishchynskiy91@gmail.com>
2018-04-24 18:57:51 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+$('#box1,#box2,#box3').click(function (event) {
+    var $el = event.target;
+    event.stopPropagation();
+    $el.style.backgroundColor = "yellow";
+    setTimeout(() => {
+        $el.style.backgroundColor = '';
+    }, 1000);
+    alert('Work stopPropagation method and stop bubbling in parent element.');
+});
+
+$('#box11,#box22,#box33').click(function (event) {
+    var $el = event.target;
+    $el.style.backgroundColor = "yellow";
+    setTimeout(() => {
+        $el.style.backgroundColor = '';
+    }, 1000);
+    alert('Without stopPropagation method and bubbling are in parent element.');
+});
+
+$('.url').click(function (event) {
+    var $el = event.target;
+    event.preventDefault();
+})
